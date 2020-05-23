dd5907036332b66fc622dc4ee144441ac5261885
dmitrishinbv <60619713+dmitrishinbv@users.noreply.github.com>
2020-05-23 18:24:33 +0300

    feat: task#2 week#4 done

--- js/table.js
+++ js/table.js
@@ -1,45 +1,148 @@
+import {createHtmlElement} from "./main.js";
+
+const sortButtonsIcons = ["fa-sort", "fa-sort-up", "fa-sort-down"];
+let columnSortStates = [];  // 0 - default, 1 - sort up, 2 - sort down
+
+
 function DataTable(config, data) {
+    let sortData = data.slice();
     let table = document.createElement("table");
-    document.getElementById(config.parent.substr(1, config.parent.length)).appendChild(table);
+    document.querySelector(config.parent).appendChild(table);
     table.setAttribute("id", "table1");
-    let n = table.appendChild(addHeader(table, config));
-    console.log(n);
+    table.appendChild(addHeader(config)); // add column names to the table head
+    columnSortStates = addSortBtns(config, table, sortData, data); // add sort buttons to the table head
+    renderTable(table, config, data);
 }
 
-function addHeader (table, config) {
+function addHeader(config) {
     let thead = document.createElement("thead");
-    // table.appendChild(thead);
     let tr = document.createElement("tr");
     thead.appendChild(tr);
-    let th;
+
     for (let i = 0; i < config.columns.length; i++) {
-      th =  (config.columns[i].type === "number") ?
-          createHtmlElement("th", tr, config.columns[i].title, null, "align-right") :
-          createHtmlElement("th", tr, config.columns[i].title, null);
+        (config.columns[i].type === "number") ?
+            createHtmlElement("th", tr, config.columns[i].title, null, "align-right") :
+            createHtmlElement("th", tr, config.columns[i].title, null, null);
     }
+
     return thead;
 }
 
-function createHtmlElement (tagName, parent, innerText = null, tagArrAttr = null, tagClass = null) {
-    let newTag = document.createElement (tagName);
-    if (innerText !== null) {
-        let text = document.createTextNode(innerText);
-        newTag.appendChild(text);
+
+function renderTable(table, config, data) {
+    let tbody = document.createElement("tbody");
+    let index = 0;
+    data.forEach((dataRow) => {
+        tbody.appendChild(buildTableDataRows(config, dataRow, ++index));
+    });
+
+    table.appendChild(tbody);
+
+    return table;
+}
+
+
+function buildTableDataRows(config, dataRow, index) {
+    let tr = document.createElement("tr");
+
+    for (let i = 0; i < config.columns.length; i++) {
+        if (config.columns[i].value === "_index") {
+            createHtmlElement("td", tr, index, null, "align-center");
+            continue;
+        }
+
+        let innerHtml = dataRow[config.columns[i].value];
+
+        (config.columns[i].type === "number") ?
+            createHtmlElement("td", tr, innerHtml, null, "align-right") :
+            createHtmlElement("td", tr, innerHtml, null, null);
     }
 
-    if (tagArrAttr !== null) {
-        tagArrAttr.forEach ((value, key, map) => {
-            newTag.setAttribute(`${key}`, `${value}`);
+    return tr;
+}
+
+function addSortBtns(config, table, sortData, data) {
+    let columnSortStates = [];
+
+    for (let i = 0; i < config.columns.length; i++) {
+        let th = table.querySelectorAll("th");
+        columnSortStates.push(0);
+        if (config.columns[i].sortable === true) {
+            let btn = createHtmlElement("i", th[i], null, new Map([["id", i]]),
+                "fas " + sortButtonsIcons[0]);
+
+            btn.onclick = () => {
+                columnSortStates[i] = changeSortState(config, btn, i);
+
+                if (columnSortStates[i] === 1) {
+                    sortData = sortColumn(config.columns[i], 1, sortData);
+                }
+
+                if (columnSortStates[i] === 2) {
+                    sortData = sortColumn(config.columns[i], -1, sortData);
+                }
+
+                if (columnSortStates[i] === 0 && (!columnSortStates.includes(1) || !columnSortStates.includes(2))) {
+                    sortData = data;
+                }
+
+                rebuildTable(table, sortData, config);
+            };
+        }
+    }
+
+    return columnSortStates;
+}
+
+
+function sortColumn(columnData, sortState, sortData) {
+    if (columnData.type === "number") {
+        sortData.sort(function (a, b) {
+            return sortState * (a[columnData.value] - b[columnData.value]);
+        });
+
+    } else {
+        sortData.sort(function (a, b) {
+            return sortState * a[columnData.value].localeCompare(b[columnData.value]);
         });
     }
 
-    if (tagClass !== null) {
-        newTag.className = tagClass;
+    return sortData;
+}
+
+
+function rebuildTable(table, sortData, config) {
+    document.querySelector(config.parent + " tbody").remove();
+    renderTable(table, config, sortData);
+}
+
+
+function changeSortState(config, btn, columnIndex) {
+    let btnClasses = btn.classList;
+    let prevState = columnSortStates[columnIndex];
+    let newState = (prevState === 0 || prevState === 1) ? ++prevState : 0;
+
+    for (let i = 0; i < btnClasses.length; i++) {
+        if (btnClasses[i].includes("fa-")) {
+            btnClasses.remove(btnClasses[i]);
+            btnClasses.add(sortButtonsIcons[newState]);
+            break;
+        }
     }
+    actualizeBtnStatuses(config, columnIndex);
 
-    (parent === null) ? document.appendChild (newTag) : parent.appendChild (newTag);
+    return newState;
+}
 
-    return newTag;
+function actualizeBtnStatuses(config, columnIndex) {
+    const table = document.querySelector(config.parent);
+    let sortBtnArr = table.querySelectorAll('thead i');
+
+    [].forEach.call(sortBtnArr, (btn => {
+        if (Number(btn.id) !== columnIndex) {
+            btn.className = "fas " + sortButtonsIcons[0];
+        }
+    }));
 }
 
 const config1 = {
@@ -47,8 +150,10 @@ const config1 = {
     columns: [
         {title: '№', value: '_index'},
         {title: 'Имя', value: 'name'},
-        {title: 'Фамилия', value: 'surname'},
-        {title: 'Возраст', value: 'age', type: 'number'},
+        //      {title: 'Фамилия', value: 'surname'},
+        {title: 'Фамилия', value: 'surname', sortable: true},
+//        {title: 'Возраст', value: 'age', type: 'number'},
+        {title: 'Возраст', value: 'age', type: 'number', sortable: true},
     ]
 };
 
