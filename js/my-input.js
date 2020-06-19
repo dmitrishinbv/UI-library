0c023372424cf5e250674fa11e1e215e798b2278
dmitrishinbv <60619713+dmitrishinbv@users.noreply.github.com>
2020-06-19 12:24:29 +0300

    week #6 task#3 complete

--- js/my-input.js
+++ js/my-input.js
@@ -11,10 +11,17 @@ Vue.component('my-input', {
             type: String,
             required: true,
         },
+        name: {
+            type: String,
+            required: true,
+        },
         required: {
             type: Boolean,
             required: false,
             default: false
+        },
+        pattern: {
+            required: false,
         },
     },
     template: `
@@ -22,12 +29,57 @@ Vue.component('my-input', {
   <label :for="title">{{ this.title }}
    <span v-if="required">*</span><br>
    </label>
-  <input :type= "type" 
+  <input      
+  @focus="changeInput"
+  @blur="checkPattern"
+    :type= "type" 
     :placeholder="placeholder" 
     :required="required"
+    :name="name"
+    :class="{'invalid': isInvalid, 'input-error': isInputError}"
     >
 </div>
-`
+`,
+    methods: {
+        checkPattern: function (event) {
+            if (!event.target.value) {
+                this.isInputError = false; //placeholder text always is correct
+            }
+
+            else {
+                if (this["name"] === "email") {
+                    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    this.isInputError = !re.test(event.target.value);
+                }
+
+                if (this["name"] === "age") {
+                    const age = event.target.value;
+                    this.isInputError = !age.length || age < 0 || age > 130;
+                }
+
+                if (this["name"] === "name") {
+                    const re = /^[A-Za-zА-Яа-яІіЄєЇїҐґ|\s]+$/;
+                    this.isInputError = !re.test(event.target.value);
+                }
+            }
+
+            this.isInvalid = (event.target.required && !event.target.value);
+          },
+
+        // reset all errors if input in focus
+        changeInput: function (event) {
+            this.isInvalid = false;
+            this.isInputError = false;
+        }
+    },
+
+    data() {
+        return {
+            answers: this.value,
+            isInvalid: false,
+            isInputError: false,
+        }
+    },
 });
 
 
