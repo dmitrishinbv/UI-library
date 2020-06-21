45fe86b17c0ce6dae02c092773f08e93cae374d9
dmitrishinbv <60619713+dmitrishinbv@users.noreply.github.com>
2020-06-21 21:39:54 +0300

    week #6 task#4-5 complete

--- js/my-input.js
+++ js/my-input.js
@@ -1,12 +1,9 @@
+const MIN_AGE = 0;
+const MAX_AGE = 130;
+const ERROR_MSG_TEXT = "Field has incorrect value or illegal symbols";
+
 Vue.component('my-input', {
     props: {
-        type: {
-            type: String,
-        },
-        placeholder: {
-            type: String,
-            required: false,
-        },
         title: {
             type: String,
             required: true,
@@ -15,71 +12,78 @@ Vue.component('my-input', {
             type: String,
             required: true,
         },
+        placeholder: {
+            type: String,
+            required: false,
+        },
         required: {
             type: Boolean,
             required: false,
             default: false
         },
         pattern: {
+            type: String,
             required: false,
         },
+        value: {
+            type: String
+        },
+        error: {
+            type: String,
+            default: ERROR_MSG_TEXT,
+            required: false
+        }
     },
+
     template: `
 <div class="input-group">
-  <label :for="title">{{ this.title }}
+  <label :for="title">{{ title }}
    <span v-if="required">*</span><br>
    </label>
   <input      
-  @focus="changeInput"
-  @blur="checkPattern"
-    :type= "type" 
+  @focus="discardErrors"
+  @blur="checkPattern($event.target.value, $event.target.required, $event.target.pattern)"
+  @input="updateValue($event.target.value)"
     :placeholder="placeholder" 
     :required="required"
     :name="name"
+    :pattern="pattern"
     :class="{'invalid': isInvalid, 'input-error': isInputError}"
     >
+    <span v-if="isInputError" class="error-msg">{{error}}</span>   
 </div>
 `,
+
     methods: {
-        checkPattern: function (event) {
-            if (!event.target.value) {
-                this.isInputError = false; //placeholder text always is correct
+        checkPattern: function (value, required, pattern) {
+            console.log(pattern);
+            if (!value) {
+                this.isInputError = false; //placeholder text is always correct
+
+            } else {
+                pattern = new RegExp(pattern);
+                     this.isInputError = (this["name"] === "age")
+                    ? !pattern.test(value) || value < MIN_AGE || value > MAX_AGE
+                    : !pattern.test(value);
             }
 
-            else {
-                if (this["name"] === "email") {
-                    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                    this.isInputError = !re.test(event.target.value);
-                }
+            this.isInvalid = (required && !value);
+        },
 
-                if (this["name"] === "age") {
-                    const age = event.target.value;
-                    this.isInputError = !age.length || age < 0 || age > 130;
-                }
+        updateValue: function (value) {
+            this.$emit('input', value);
+        },
 
-                if (this["name"] === "name") {
-                    const re = /^[A-Za-zА-Яа-яІіЄєЇїҐґ|\s]+$/;
-                    this.isInputError = !re.test(event.target.value);
-                }
-            }
-
-            this.isInvalid = (event.target.required && !event.target.value);
-          },
-
-        // reset all errors if input in focus
-        changeInput: function (event) {
-            this.isInvalid = false;
-            this.isInputError = false;
+        // discards all errors when input filed in focus
+        discardErrors: function () {
+            this.isInvalid = this.isInputError = false;
         }
     },
-
     data() {
         return {
-            answers: this.value,
-            isInvalid: false,
-            isInputError: false,
+            inputValues: this.value,
+            isInvalid: false, //true if required but empty field
+            isInputError: false, //true if field has incorrect value or illegal symbols
         }
     },
 });
-
-
